87e8e804b7a30fb88e9dc68b651598cf876e4d89
Vinícius <viniciushallmann2015@gmail.com>
2023-09-16 01:49:25 -0300

    att classeMeuPerfil

--- classeMeuPerfil.py
+++ classeMeuPerfil.py
@@ -4,12 +4,13 @@ from conexaoBD import database
 
 
 class MeuPerfilPage:
-    def __init__(self, master, id_userLogged, app):
+    def __init__(self, master, id_user_logged, app):
         self.master = master
-        self.id_user_logged = id_userLogged
+        self.id_user_logged = id_user_logged
         self.app = app  
         self.app.title("Meu Perfil")
-
+        self.change_password_window = None
+        
         db = database()
         self.user = db.return_user(self.id_user_logged)
 
@@ -19,22 +20,37 @@ class MeuPerfilPage:
         self.country = self.user[7]
         self.email = self.user[4]
 
-        user_info_label = tk.Label(self.app, text=f"Nome do Usuário: {self.name} \n Seguidores:{self.followers} \tSeguindo: {self.following} \n País {self.country} \nEmail {self.following}").pack()
-        user_my_list = tk.Button(self.app, text="Minha Lista").pack()
-'''
-    def user_name(self):
-        return "USER_NAME"
-    def user_followers( self ):
-        return "10"
-    def user_following( self ):
-        return "10"
-    def user_country( self ):
-        return "USER_COUNTRY"
-    def user_email(self):
-        return "USER_EMAIL"
+        user_info_label = tk.Label(self.app, text=f"Nome do Usuário: {self.name} \n Seguidores:{self.followers} \tSeguindo: {self.following} \n País {self.country} \nEmail {self.email}").pack()
+        user_my_list = tk.Button(self.app, text="Minhas Listas").pack()
+        user_alter_password = tk.Button(self.app, text="Alternar Senha", command=self.change_password).pack()
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    id_userLogged = 1 
-    app = MeuPerfil(root, id_userLogged, None)  
-    root.mainloop() '''
+    def change_password(self):
+        self.change_password_window = tk.Toplevel(self.app)
+        self.change_password_window.geometry("300x150")
+        self.change_password_window.title("Alterar Senha")
+        
+        password_label = tk.Label(self.change_password_window , text="Senha atual: ")
+        password_label.pack()
+        self.password_entry = tk.Entry(self.change_password_window , width=30, show="*")
+        self.password_entry.pack()
+
+        new_password_label = tk.Label(self.change_password_window , text="Nova senha: ")
+        new_password_label.pack()
+        self.new_password_entry = tk.Entry(self.change_password_window , width=30, show="*")
+        self.new_password_entry.pack()
+
+        self.new_password = self.new_password_entry.get()
+        confirm_password_button = tk.Button(self.change_password_window, text="Confirmar Senha", command=self.confirm_password)
+        confirm_password_button.pack()
+
+    def confirm_password(self):
+        password = self.password_entry.get()
+        db = database()
+        result = db.validate_password(self.id_user_logged, password)[0]
+        if result is not None:
+                db.update_password(self.id_user_logged, self.new_password_entry.get())
+                tk.messagebox.showinfo("Nome do App", "OK")
+                self.change_password_window.destroy()
+        else:
+                #PARA DEPOIS, SE A SENHA ATUAL NÃO ESTIVER CORRETA MOSTRAR MENSAGEM DE ERRO
+                tk.messagebox.showerror("Nome do App", "Credenciais inválidas. Tente novamente.")
