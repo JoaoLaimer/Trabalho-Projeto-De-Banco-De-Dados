40a8f5cafc464841196b465193a8ed69c7c9ffff
Vinícius <viniciushallmann2015@gmail.com>
2023-09-16 13:37:29 -0300

    commit chamada minha lista e cria lista

--- classeApp.py
+++ classeApp.py
@@ -5,6 +5,8 @@ from classeLogin import LoginPage
 from classeRegistro import RegistroPage
 from classeMeuPerfil import MeuPerfilPage
 from classeBusca import BuscaPage
+from classeMinhasListas import MinhasListasPage
+from classeCriarLista import CriarListaPage
 
 class App:
     def __init__(self, master):
@@ -48,6 +50,12 @@ class App:
         self.busca_button = tk.Button(self.master, text="Busca", command=self.abrir_pagina_busca)
         self.busca_button.grid(row=0, column=1, padx=10, pady=10)
 
+        self.minha_lista_button = tk.Button(self.master, text="Minhas Listas", command=self.abrir_pagina_minhas_listas)
+        self.minha_lista_button.grid(row=0, column=2, padx=10, pady=10)
+
+        self.cria_lista_button = tk.Button(self.master, text="Criar Lista", command=self.abrir_pagina_criar_lista)
+        self.cria_lista_button.grid(row=0, column=3, padx=10, pady=10)
+
     def abrir_pagina_perfil(self):
         perfil_window = tk.Toplevel(self.master)
         perfil_page = MeuPerfilPage(self,self.user_id, perfil_window)
@@ -55,7 +63,14 @@ class App:
     def abrir_pagina_busca(self):
         busca_window = tk.Toplevel(self.master)
         busca_page = BuscaPage(self, busca_window)
+    
+    def abrir_pagina_minhas_listas(self):
+        lista_window = tk.Toplevel(self.master)
+        lista_page = MinhasListasPage(self,self.user_id, lista_window)
 
+    def abrir_pagina_criar_lista(self):
+        cria_lista_window = tk.Toplevel(self.master)
+        cria_lista_page = CriarListaPage(self,self.user_id, cria_lista_window)
 
 if __name__ == "__main__":
     root = tk.Tk()
