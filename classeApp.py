64304f5c351ecf451f0a2c5afb69aae82fc9c6a5
Vinícius <viniciushallmann2015@gmail.com>
2023-09-17 01:19:31 -0300

    corrigido bug do app nao fechar

--- classeApp.py
+++ classeApp.py
@@ -11,35 +11,27 @@ from classeCriarLista import CriarListaPage
 from classeFilme import FilmePage
 import tkinter.messagebox
 
-
 customtkinter.set_appearance_mode("light") 
 customtkinter.set_default_color_theme("blue")  
-
 
 class App(customtkinter.CTk):
 
     def __init__(self, master):
         super().__init__()
-        
         self.master = master
         self.master.geometry("320x400")
         self.master.title("Nome do App")
-        self.master.configure(bg="lightgray")
-        self.button_style = ("Arial", 28)
         self.botao_registro_filme = None  # none pq ele nao aparece
         self.botao_login_registro = None
-
         # botao pra tela de login
         self.login_button = customtkinter.CTkButton(master, text="Login", command=self.abrir_pagina_login)
         self.login_button.grid(row=0, column=0, padx=10, pady=10)
         
         self.signup_button = customtkinter.CTkButton(master, text="Sign Up", command=self.abrir_pagina_registro)
-        self.signup_button.grid(row=0, column=1, padx=10, pady=10)
-
+        self.signup_button.grid(row=1, column=0, padx=10, pady=10)
 
         self.user_id_loggado = None
         
-
     def abrir_pagina_login(self):
         login_window = tk.Toplevel(self.master)
         login_page = LoginPage(login_window, self)
@@ -62,7 +54,6 @@ class App(customtkinter.CTk):
     def create_landing_page(self):
         self.master.geometry("320x400")
         
-        
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
@@ -76,16 +67,8 @@ class App(customtkinter.CTk):
         self.minha_lista_button = customtkinter.CTkButton(self.master, text="Minhas Listas", command=lambda :self.exibir_lista_filmes(self.user_id_loggado, True))
         self.minha_lista_button.grid(row=4, column=0, padx=20, pady=10)
 
-<<<<<<< HEAD
-        self.minha_lista_button = tk.Button(self.master, text="Minhas Listas", command=lambda :self.exibir_lista_filmes(self.user_id_loggado, True))
-        self.minha_lista_button.grid(row=0, column=2, padx=10, pady=10)
-
-        self.cria_lista_button = tk.Button(self.master, text="Criar Lista", command=self.abrir_pagina_criar_lista)
-        self.cria_lista_button.grid(row=0, column=3, padx=10, pady=10)
-=======
         self.cria_lista_button = customtkinter.CTkButton(self.master, text="Criar Lista", command=self.abrir_pagina_criar_lista)
         self.cria_lista_button.grid(row=5, column=0, padx=10, pady=10)
->>>>>>> 91e5745453fa1051049e4ab7e1bbc5f6aa27dea1
 
     def abrir_pagina_perfil(self,user_id):
         perfil_window = tk.Toplevel(self.master)
@@ -102,11 +85,9 @@ class App(customtkinter.CTk):
     def abrir_pagina_criar_lista(self):
         cria_lista_window = tk.Toplevel(self.master)
         cria_lista_page = CriarListaPage(self,self.user_id_loggado, cria_lista_window)
-
-
     
-
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
+    root.protocol("WM_DELETE_WINDOW", root.quit)
     root.mainloop()
