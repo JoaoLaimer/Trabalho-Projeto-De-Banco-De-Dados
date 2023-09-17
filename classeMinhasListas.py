de773bdcee1859a11569c793a352729941f805c4
Vinícius <viniciushallmann2015@gmail.com>
2023-09-17 16:30:30 -0300

    commit att minha lista

--- classeMinhasListas.py
+++ classeMinhasListas.py
@@ -29,56 +29,28 @@ class MinhasListasPage:
         self.list_lists()
 
     def list_lists(self):
-        options_logged_profile = ["Adicionar filme", "Excluir filme", "Apagar lista"]
-        self.options_var = customtkinter.StringVar(value=options_logged_profile[0])
         
-        option_menu = customtkinter.CTkOptionMenu(self.app,
-                                        values=options_logged_profile,
-                                        variable=self.options_var,
-                                        width=100,
-                                        corner_radius=5,
-                                        text_color=("black", "white"),
-                                        dropdown_text_color=("black", "white"),
-                                        font=("Arial", 12))
-        option_menu.grid(row=0, column=2, padx=10, pady=10)
-        checkbox_vars = []
-        rowIncrement = 2
         db = database()
         lists = db.return_user_lists(self.id_user)
-        liked_lists = db.return_liked_lists(self.id_user)
-
-        #print(lists)
-        #print(len(lists))
-        #for likes in liked_lists:
-            #for item in likes:
-                #print(item)
-            
         self.like_button = [None for _ in range(len(lists))]
-        #self.unlike_button = [None for _ in range(len(lists))]
+
         for i in range(len(lists)):
             list_name = lists[i][1]
+            #list_id = lists[i][3]
             self.list_id = lists[i][3]
             list_label = [None for _ in range(len(lists))]
 
-            list_label[i] = customtkinter.CTkButton(self.app, text=f"Nome: {list_name}", command=lambda: self.show_movies_in_list(self.list_id))
-            list_label[i].grid(row = i, column = 0, padx = 10, pady = 10)
-                
+            
             if self.bool:
-                # list_menu = tk.OptionMenu(self.app, self.options_var, *options_logged_profile)
-                # list_menu.grid(row=i, column=1, padx=10, pady=10)
-                options_logged_profile = ["Adicionar filme", "Excluir filme", "Apagar lista"]
-                self.options_var = customtkinter.StringVar(value=options_logged_profile[0])
-                
-                option_menu = customtkinter.CTkOptionMenu(self.app,
-                                                values=options_logged_profile,
-                                                variable=self.options_var,
-                                                width=100,
-                                                corner_radius=5,
-                                                text_color=("black", "white"),
-                                                dropdown_text_color=("black", "white"),
-                                                font=("Arial", 12))
-                option_menu.grid(row=i, column=2, padx=10, pady=10)
+                list_label[i] = customtkinter.CTkButton(self.app, text=f"Nome: {list_name}", command=lambda list_id = self.list_id: self.show_movies_in_list(list_id, bool))
+                list_label[i].grid(row = i, column = 0, padx = 10, pady = 10)
+                """self.remove_button = customtkinter.CTkButton(self.app, text="Remover", command=lambda list_id = self.list_id: self.remove_movie(list_id))
+                self.remove_button.grid(row=i, column=1, padx=10, pady=10)
+                self.delete_list_button = customtkinter.CTkButton(self.app, text="Apagar lista", command=lambda list_id = self.list_id: self.delete_list(list_id))
+                self.delete_list_button.grid(row=i, column=2, padx=10, pady=10)"""
             else:
+                list_label[i] = customtkinter.CTkButton(self.app, text=f"Nome: {list_name}", command=lambda list_id = self.list_id: self.show_movies_in_list(list_id))
+                list_label[i].grid(row = i, column = 0, padx = 10, pady = 10)
                 if not db.check_like(self.list_id, self.id_user):
                     self.like_button[i] = customtkinter.CTkButton(self.app, text="Curtir", command=lambda list_id = self.list_id, row = i: self.like_event(list_id,row))
                     self.like_button[i].grid(row=i, column=2, padx=10, pady=10)
@@ -99,36 +71,26 @@ class MinhasListasPage:
         self.like_button[pos].grid_remove()
         self.like_button[pos] = customtkinter.CTkButton(self.app, text="Curtir", command=lambda list_id = id_list, row=pos: self.like_event(list_id,row))
         self.like_button[pos].grid(row=pos, column=2, padx=10, pady=10)
-    
-    # def update_like_button(self, id_list, liked):
-    #     if liked:
-    #         button_text = "Descurtir"
-    #         command = lambda id_list=id_list: self.unlike_event(id_list)
-    #     else:
-    #         button_text = "Curtir"
-    #         command = lambda id_list=id_list: self.like_event(id_list)
-
-    #     if not self.like_button:
-    #         self.like_button = customtkinter.CTkButton(self.app, text=button_text, command=command)
-    #     else:
-    #         self.like_button.configure(text=button_text, command=command)
-
-    #     self.like_button.configure(text=button_text, command=command)
-
-        # Force a redraw of the button
-        # self.like_button.update_idletasks()
-
             
-    def show_movies_in_list(self, id_list):
+    def show_movies_in_list(self, id_list, bool):
         db = database()
         movies = db.return_movies_in_list(id_list)
 
         movie_window = tk.Toplevel(self.app)
         movie_window.title("Filmes da Lista")
 
-        for movie in movies:
-            movie_id = movie[0]
-            movie_name = db.get_movie_name(movie_id) 
-            movie_label = tk.Label(movie_window, text=movie_name)
-            movie_label.grid(row = rowIncrement, column = 0, padx = 10, pady = 10)
-            rowIncrement += 1
+        print(movies)
+        """for i in range(len(movies)):
+            movie_id = movies[i][0]
+            movie_title = db.get_movie_name(movie_id) 
+            movie_label = tk.Label(movie_window, text=movie_title)
+            movie_label.grid(row = i, column = 0, padx = 10, pady = 10)"""
+
+        delete_list_button = customtkinter.CTkButton(movie_window, text="Apagar lista", command=lambda list_id = id_list: self.delete_list(list_id))
+        delete_list_button.grid(row=1, column=2, padx=10, pady=10)
+
+    def delete_list(self, id_list):
+        db = database()
+        db.delete_list(id_list)
+        messagebox.showinfo("Sucesso", "Lista apagada com sucesso!")
+        self.app.destroy()
